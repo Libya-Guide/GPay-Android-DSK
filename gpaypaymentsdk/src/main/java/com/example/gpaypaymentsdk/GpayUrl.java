cc4dafa1d328fb14922a848004276594cd66546d
Esra Elha <esoelbo97@gmail.com>
2025-05-26 12:04:28 +0200

    Update GpayUrl.java

--- gpaypaymentsdk/src/main/java/com/example/gpaypaymentsdk/GpayUrl.java
+++ gpaypaymentsdk/src/main/java/com/example/gpaypaymentsdk/GpayUrl.java
@@ -1,8 +1,8 @@
 package com.example.gpaypaymentsdk;
 
 public enum GpayUrl {
-    TESTING_STAGE("https://gpay-staging.libyaguide.net/banking/"),
-    PRODUCTION("https://gpay.ly/banking/");
+    TESTING_STAGE("https://gpay-staging.libyaguide.net/banking/gpay_payment_page.jsp"),
+    PRODUCTION("https://gpay.ly/banking/gpay_payment_page.jsp");
     private final String url;
     GpayUrl(String url) {
         this.url = url;
